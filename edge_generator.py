5769a4b4f0d38013436fbcb6b64a29e6c7ddb30f
Sanghyun <sanghyun@xbrain.team>
2023-02-21 20:08:01 +0900

    modify edge_generator.py

--- edge_generator.py
+++ edge_generator.py
@@ -5,6 +5,7 @@ git repo: https://github.com/Karel911/TRACER
 """
 import os
 import cv2
+from PIL import Image
 import numpy as np
 from tqdm import tqdm
 
@@ -19,15 +20,15 @@ def edge_generator(dataset):
     mask_list = os.listdir(mask_path)
 
     for i, img_name in tqdm(enumerate(mask_list)):
-        mask = cv2.imread(mask_path + img_name)
-        mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+        mask = np.array(Image.open(mask_path + img_name))
+        mask = mask * 255
         mask = np.int64(mask > 128)
 
         [gy, gx] = np.gradient(mask)
         tmp_edge = gy * gy + gx * gx
         tmp_edge[tmp_edge != 0] = 1
         bound = np.uint8(tmp_edge * 255)
-        cv2.imwrite(os.path.join(save_path, f'{img_name}'), bound)
+        cv2.imwrite(os.path.join(save_path, img_name.split('_mask')[0] + '.png'), bound)
 
 
 if __name__ == '__main__':
